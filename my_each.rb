0daabde07f84b4c89e7b8f5f6d90a959c3b885e6
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 14:38:55 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,12 @@
 def my_each(array)
   i = 0 
   if array != []
-  while i < array.length
-    yield(print_item(array, i))
-    i = i + 1
-  end
+    while i < array.length
+      yield(print_item(array, i))
+      i = i + 1
+    end
+  else
+    puts "Nothing in the array"
 end
 
 def print_item(array, index_spot)
