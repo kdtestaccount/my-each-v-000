b65e4e4cc125c23479494583950655eb116a0b39
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 14:37:23 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0 
+  while i < array.length
+    yield(print_item(array, i))
+    count = i + 1
+  end
+end
+
+def print_item(array, index_spot)
+  object = array[index_spot]
+  object
 end
