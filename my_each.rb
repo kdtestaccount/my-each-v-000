dec2df066973cbd510511d03b0db91140e4e4a11
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 14:38:09 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array)
   i = 0 
   while i < array.length
-    yield(array[1])
+    yield(print_item(array, i))
     i = i + 1
   end
 end
@@ -9,15 +9,4 @@ end
 def print_item(array, index_spot)
   object = array[index_spot]
   object
-end
-
-def hello_t(array)
-  i = 0
- 
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
- 
-  array
 end
